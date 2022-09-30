f34ccefc87001370b562d0ed7d135b1bb8841c7b
adamantdude <adam.plee23@gmail.com>
2022-09-30 14:21:42 -0500

    update .json with start command ; update stylesheet to format the calculator ; update index.html with a few lines of code to make it look pretty ; client.js update for render function and expression object

--- server/public/client.js
+++ server/public/client.js
@@ -1,6 +1,12 @@
 
 $(main);
 
+const expression = {
+    val1: 0,
+    val2: 0,
+    operator: '+'
+}
+
 const opEnum = {
     '+':0,'-':1,'*':2,'/':3,'%':4,'=':5
 }
@@ -12,9 +18,15 @@ function main() {
 }
 
 function whichButton() {
-    
 
-    switch(opEnum[$(this).attr('id')]) {
+    expression.operator = $(this).attr('id');
 
-    }
+    update();
+
 }
+
+function update() {
+    $('#operator').text(expression.operator);
+}
+
+// function 
