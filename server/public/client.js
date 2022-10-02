5c56422293fc0e0685c330ac8b5ca631ff647a30
adamantdude <adam.plee23@gmail.com>
2022-10-02 17:13:39 -0500

    implement history of the calculator

--- server/public/client.js
+++ server/public/client.js
@@ -7,42 +7,49 @@ const calcObj = {
     val1: 0,
     val2: 0,
     operator: '+',
-    sum: 0
+    sum: 0,
 }
+
+let history = [];
+
+console.log(history.length);
 
 const allowedSet = [46, 48, 57];
-
-const opEnum = {
-    '+':0,'-':1,'*':2,'/':3,'%':4,'=':5
-}
 
 function main() {
 
     $(document).on('click', 'button', whichButton);
 
+    $(document).on('click', '.historyItem', historyRemove);
+
     // $('input').on('keypress', typeSet);
 
-}
+    historyLoad();
+
+} // END main()
 
 function whichButton() {
 
     calcObj.val1 = $('#value1').val();
     calcObj.val2 = $('#value2').val();
 
-    calcObj.operator = $(this).attr('id');
+    if($(this).attr('id') != '=') {
+        calcObj.operator = $(this).attr('id');
+    }
 
-    (calcObj.operator == '=' && (calcObj.val1 && calcObj.val2)) ? sendVals() : update();
+    ($(this).attr('id') == '=' && (calcObj.val1 && calcObj.val2)) ? sendVals() : render();
 
-}
+} // END whichButton()
 
-function update() {
+function render() {
 
-    if(operator != '=') {
+    if(calcObj.operator != '=') {
         $('#operator').text(calcObj.operator);
     }
     
     $('#sum').text(calcObj.sum);
-}
+
+} // END render()
 
 function sendVals() {
     
@@ -59,24 +66,66 @@ function sendVals() {
             console.log('client.js /calculate POST error', err);
         })
 
-}
+} // END sendVals()
 
 function receiveResult() {
 
-$.ajax({
-    url: '/calculate',
-    method: 'GET'
-})
-    .then((res) => {
-        console.log('client.js /calculate GET', res);
-        calcObj.sum = res;
-        update();
+    $.ajax({
+        url: '/calculate',
+        method: 'GET'
     })
-    .catch((err) => {
-        console.log('client.js /calculate GET error', err);
-    })
+        .then((res) => {
+            console.log('client.js /calculate GET', res);
+            calcObj.sum = res.sum;
+            historyLoad();
+        })
+        .catch((err) => {
+            console.log('client.js /calculate GET error', err);
+        })
 
-}
+    console.log('the sum is', calcObj.sum);
+
+} // END receiveResult()
+
+function historyLoad() {
+
+    $.ajax({
+        url: '/history',
+        method: 'GET'
+    })
+        .then((res) => {
+            console.log('in /history GET', res);
+
+            history = res;
+
+            $('#history').empty();
+            for(let i=0; i<history.length; ++i) {
+                $('#history').append(`
+                    <p class="historyItem" id="${i}">${history[i].val1} ${history[i].operator} ${history[i].val2} = ${history[i].sum}</p><hr>
+                `)
+            }
+
+            render();
+        })
+} // END historyLoad()
+
+function historyRemove() {
+
+    $.ajax({
+        url: '/history',
+        method: 'POST',
+        data: {
+            index: $(this).attr('id')
+        }
+    })
+        .then((res) => {
+            console.log('in /history POST', res);
+            historyLoad();
+        })
+        .catch((err) => {
+            console.log('in /history POST error', err);
+        })
+} // END historyRemove()
 
 // function typeSet(evt) {
 //     console.log('Typed something', $(this));
--- server/server.js
+++ server/server.js
@@ -12,20 +12,30 @@ app.listen(5000, () => {
 
 const history = [];
 
-const calcObj = {
+let calcObj = {
     val1: 0,
     val2: 0,
     operator: '+',
     sum: 0,
 }
 
+console.log(calcObj);
+
 // ------------------------------------------
 
 // GET BEGIN
 app.get('/calculate', (req, res) => {
     console.log('server.js /calculate GET');
+    console.log(calcObj);
 
     res.send(calcObj);
+})
+
+app.get('/history', (req, res) => {
+    console.log('server.js /history GET');
+    console.log(history);
+
+    res.send(history);
 })
 // GET END
 
@@ -37,9 +47,19 @@ app.post('/calculate', (req, res) => {
 
     history.push(req.body);
 
-    calc(req.body);
+    calcObj = req.body;
+
+    calc();
 
     res.sendStatus(201);
+})
+
+app.post('/history', (req, res) => {
+    console.log('server.js /history POST');
+
+    history.splice(req.body.index, 1);
+
+    res.sendStatus(200);
 })
 // POST END
 
@@ -47,6 +67,22 @@ app.post('/calculate', (req, res) => {
 
 // FUNCTIONS
 
-function calc(obj) {
-    obj.sum 
+function calc() {
+    switch(calcObj.operator) {
+        case '+':
+            calcObj.sum = Number(calcObj.val1) + Number(calcObj.val2);
+            break;
+        case '-':
+            calcObj.sum = Number(calcObj.val1) - Number(calcObj.val2);
+            break;
+        case '/':
+            calcObj.sum = Number(calcObj.val1) / Number(calcObj.val2);
+            break;
+        case '%':
+            calcObj.sum = Number(calcObj.val1) % Number(calcObj.val2);
+            break;
+        case '*':
+            calcObj.sum = Number(calcObj.val1) * Number(calcObj.val2);
+            break;
+    }
 }
